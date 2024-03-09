683df35929777231926b591ef0c10a805c4179ee
kimberlyssean ss <tarjo.12simin@gmail.com>
2024-03-09 16:17:00 +0000

    Version 1.0.1

--- testvalid.js
+++ testvalid.js
@@ -2,6 +2,7 @@ const chalk = require("chalk")
 
 function testValid() {
     console.log(chalk.red("Check If True"));
+    console.log(chalk.green("Well done its work"));
 }
 
 module.exports = testValid
